8c1cf8cc81f6ad6f7907883e9d7f063aa1ec2655
Mazin Fouad <m.fouad@live.de>
2022-07-13 18:03:33 +0200

    winner determined

--- script.js
+++ script.js
@@ -1,13 +1,66 @@
 let fields = [];
-let currentShape = 'Cross';
+let currentShape = 'cross';
 
 function fillShape(id) {
-  if (currentShape == 'Cross') {
-    currentShape = 'Circle';
+  if (currentShape == 'cross') {
+    currentShape = 'circle';
   } else {
-    currentShape = 'Cross';
+    currentShape = 'cross';
   }
 
   fields[id] = currentShape;
   console.log(fields);
+  draw();
+  checkForWinn();
+}
+
+function draw() {
+  for (let i = 0; i < fields.length; i++) {
+    if (fields[i] == 'circle') {
+      document.getElementById('circle-' + i).classList.remove('d-none');
+    }
+    if (fields[i] == 'cross') {
+      document.getElementById('cross-' + i).classList.remove('d-none');
+    }
+  }
+}
+
+function checkForWinn() {
+  let winner;
+
+  if (fields[0] == fields[1] && fields[1] == fields[2]) {
+    winner = fields[0];
+  }
+
+  if (fields[3] == fields[4] && fields[4] == fields[5]) {
+    winner = fields[3];
+  }
+
+  if (fields[6] == fields[7] && fields[7] == fields[8]) {
+    winner = fields[6];
+  }
+
+  if (fields[0] == fields[3] && fields[3] && fields[6]) {
+    winner = fields[0];
+  }
+
+  if (fields[2] == fields[4] && fields[4] == fields[7]) {
+    winner = fields[2];
+  }
+
+  if (fields[3] == fields[5] && fields[5] == fields[8]) {
+    winner = fields[3];
+  }
+
+  if (fields[0] == fields[4] && fields[4] == fields[8]) {
+    winner = fields[0];
+  }
+
+  if (fields[2] == fields[4] && fields[4] == fields[6]) {
+    winner = fields[2];
+  }
+
+  if (winner) {
+    console.log('WINN!!', winner);
+  }
 }
